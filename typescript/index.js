a1daec3c913598d072bec8eceb5890cca0f284c0
otakustay <otakustay@gmail.com>
2020-09-16 11:12:18 +0800

    fix: compatible with multiple tsconfig

--- typescript/index.js
+++ typescript/index.js
@@ -7,7 +7,7 @@ module.exports = {
                 ecmaFeatures: {
                     jsx: true,
                 },
-                project: './tsconfig.json',
+                project: './**/tsconfig.json',
             },
             plugins: ['@typescript-eslint'],
             rules: {
