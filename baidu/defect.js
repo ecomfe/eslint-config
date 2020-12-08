c922402b29f65da86a4055d92471e49554c36a6a
Gray Zhang <otakustay@gmail.com>
2020-12-08 04:00:12 +0000

    fix: disable all default errors in defect rules

--- baidu/defect.js
+++ baidu/defect.js
@@ -1,11 +1,27 @@
 const defaults = require('./default');
 
-const switchLevel = (rule, newLevel) => [newLevel, ...rule.slice(1)];
+const switchLevel = (rule, newLevel) => {
+    if (typeof rule === 'string' || typeof rule === 'number') {
+        return newLevel;
+     }
+
+     return [newLevel, ...rule.slice(1)];
+};
+
+// Disable all error rules in default, these will be reported from code style check
+const defaultRulesWithErrosDisabled = Object.entries(defaults.rules).reduce(
+    (rules, [name, config]) => {
+        const level = (typeof config === 'string' || typeof config === 'number') ? config : config[0];
+        rules[name] = (level === 'error' || level === 2) ? switchLevel(config, 'off') : config;
+        return rules;
+    },
+    {}
+);
 
 module.exports = {
     ...defaults,
     rules: {
-        ...defaults.rules,
+        ...defaultRulesWithErrosDisabled,
         'no-eval': 'error',
         'no-extra-bind': 'error',
         'no-implied-eval': 'error',
