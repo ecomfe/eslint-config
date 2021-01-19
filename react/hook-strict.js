27b49396568e4b4a5b4b0d0f58ac45da84dce9cd
susiwen8 <susiwen8@gmail.com>
2021-01-19 15:06:38 +0800

    Fix: hook strict rules

--- react/hook-strict.js
+++ react/hook-strict.js
@@ -1,9 +1,11 @@
 const defaults = require('./hook');
+const strict = require('./strict');
 
 module.exports = {
     ...defaults,
     rules: {
         ...defaults.rules,
+        ...strict.rules,
         'react-hooks/exhaustive-deps': 'error',
     },
 };
