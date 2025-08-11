3411f2598efd64973e4f76464b5e90ec6f7f7074
otakustay <otakustay@gmail.com>
2025-08-11 20:32:08 +0800

    chore: use commit-and-tag-version to replace standard-version

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'footer-max-line-length': [2, 'always', 400],
+        'body-max-line-length': [0],
+    },
+};
